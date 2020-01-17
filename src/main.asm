e4b2b7e8169aeeec5b2fddd1368f074c8be3ff61
Mason Fisher <sackzombie10@gmail.com>
2020-01-17 16:14:20 -0500

    Made a simple program!

--- src/main.asm
+++ src/main.asm
@@ -2,14 +2,28 @@
 					processor 6502
 
 			include "src/lib/vcs.asm"
-			;include "src/lib/macro.asm" ; not being used right now
+			include "src/lib/macro.asm"
 
 ; 4 kilobyte Atari 2600 cartridges start at f000
 					org $f000
 ; "start" and "loop" are labels are they are on the left margin
 
-start:		lda #$56
-					sta COLUBK
+start:		sei
+					cld
+					ldx #$ff
+					txs
+
+					ldx #$ff
+					lda #$00
+zeroZP		sta COLUBK ; Set the color of the background to black
+					dex
+					bne zeroZP
+
+					lda #$0e
+					sta COLUBK ; Set the color of the background to white
+					lda #$00
+					sta COLUBK ; Set the color of the background to black
+
 					jmp start
 
 ; Skip to address FFFC in the ROM, which is the end of a 4K cartridge
