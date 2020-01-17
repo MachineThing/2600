ff35bdb93363387fdda892829573e481830c365f
Mason Fisher <sackzombie10@gmail.com>
2020-01-17 13:59:37 -0500

    Made more room for labels

--- src/main.asm
+++ src/main.asm
@@ -1,18 +1,19 @@
 ; Tell DASM we are using 6502 instructions
-			processor 6502
+					processor 6502
 
 ; Saving these libraries for later!
 			;include "src/lib/vcs.asm"
 			;include "src/lib/macro.asm"
 
 ; 4 kilobyte Atari 2600 cartridges start at f000
-				org $f000
+					org $f000
+; "start" and "loop" are labels are they are on the left margin
 
-start:	ldx #100
-loop:		dex
-				bne loop
+start:		ldx #100
+loop:			dex
+					bne loop
 
 ; Skip to address FFFC in the ROM
-			org $fffc
-				.word start
-				.word start
+					org $fffc
+						.word start
+						.word start
