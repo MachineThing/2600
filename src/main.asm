e59a991cca783d68080490b8213ec2d9e32c5cd1
Mason Fisher <sackzombie10@gmail.com>
2020-01-17 13:43:16 -0500

    Made a loop and also made the ROM actually runnable in Stella EMU

--- src/main.asm
+++ src/main.asm
@@ -1,7 +1,14 @@
-
-; Assembler should use basic 6502 instructions
-	processor 6502
+; Tell DASM we are using 6502 instructions
+			processor 6502
 
 ; 4 kilobyte Atari 2600 cartridges start at f000
-  org $f000
-  lda #$17
+				org $f000
+
+start:	ldx #100
+loop:		dex
+				bne loop
+
+; Skip to address FFFC in the ROM
+			org $fffc
+				.word start
+				.word start
