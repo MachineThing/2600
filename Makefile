f346da10a9a7e282c1b2ff716608890438916fa0
Mason Fisher <sackzombie10@gmail.com>
2020-01-22 13:44:46 -0500

    Made Makefile to compile the new program

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 asmc=dasm
 main:
 	$(asmc) starry/starry.asm -f3 -v5 -ocompiled/starry.bin
+	$(asmc) rainbow/rainbow.asm -f3 -v5 -ocompiled/rainbow.bin
--- rainbow/rainbow.asm
+++ rainbow/rainbow.asm
@@ -3,16 +3,17 @@
 ; Created: January 22nd, 2020
 ; Modified: January 22nd, 2020
 
-; Tell DASM we are using 6502 instructions
-					processor 6502
+          processor 6502					; Tell DASM we are using 6502 instructions
 
-					include "lib/vcs.asm"
-					include "lib/macro.asm"
+          include "lib/vcs.asm"		; Atari 2600 library
+          include "lib/macro.asm"	; 2600 macross
 
 ; 4 kilobyte Atari 2600 cartridges start at f000
 					org $f000
 
+BGColor   equ $81       ; A equate that holds a background color we will use later
 
+start:    CLEAN_START   ; A macro that clears memory and TIA (Television Interface Adaptor)
 
 ; Skip to address FFFC in the ROM, which is the end of a 4K cartridge
 					org $fffc
