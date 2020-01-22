f4e343d3fd8caaad6a1573556fab13abe7aaabdc
Mason Fisher <sackzombie10@gmail.com>
2020-01-22 14:12:57 -0500

    Added more comments for the people that don't know assembly!

--- rainbow/rainbow.asm
+++ rainbow/rainbow.asm
@@ -11,17 +11,17 @@
 ; 4 kilobyte Atari 2600 cartridges start at f000
 					org $f000
 
-BGColor   equ $81       ; A equate that holds a background color we will use later
+BGColor   equ $81       ; Store this value into RAM. We will use this value later, the name gives out a hint
 
 start:    CLEAN_START   ; A macro that clears memory and TIA (Television Interface Adaptor)
 NFrame:   lda #2
-          sta VBLANK
-          sta VSYNC
+          sta VBLANK    ; Turn on Vertical Blank
+          sta VSYNC     ; Turn on Vertical Sync
 
-          sta WSYNC
+          sta WSYNC     ; Turn on Wait for SYNC
 
           lda #0
-          sta VSYNC
+          sta VSYNC     ; Turn off Vertical Sync
 
           ldx #37       ; 37 scanlines
 
@@ -30,25 +30,18 @@ LVBlank:  sta WSYNC
           bne LVBlank
 
           lda #0
-          sta VBLANK
+          sta VBLANK    ; Turn off Vertical Blank
 
           ldx #210      ; 210 scanlines (To fully cover the screen, overflows into overscan)
-          ldy BGColor
-LVScan:   sty COLUBK
-          sta WSYNC
-          iny
-          dex
-          bne LVScan
+          ldy BGColor   ; Store $81 into the Y register
+LVScan:   sty COLUBK    ; Make the background the color of the Y register
+          sta WSYNC     ; Turn off Wait for SYNC
+          iny           ; Increment Y
+          dex           ; Decrement X
+          bne LVScan    ; Jump to LVScan if X is not equal to 0
 
-          lda #2
-          sta LVBlank
-          ldx #30
-LVOver:   sta WSYNC
-          dex
-          bne LVOver
-
-          dec BGColor
-          jmp NFrame
+          dec BGColor   ; Decrement the Background Color Value
+          jmp NFrame    ; Jump to new frame
 
 ; Skip to address FFFC in the ROM, which is the end of a 4K cartridge
 					org $fffc
