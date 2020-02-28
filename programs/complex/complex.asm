eeb01520d1652091f6b80001e1c7b20b1d182a92
Mason Fisher <sackzombie10@gmail.com>
2020-02-28 00:17:29 -0500

    Got the playfield to render, the sprite broke through

--- programs/complex/complex.asm
+++ programs/complex/complex.asm
@@ -14,99 +14,149 @@ SpriteHeight	equ #10
 
 					seg.u Variables
 					org $80
+PFPtr					.word	; pointer to playfield data
+PFIndex				.byte	; offset into playfield array
+PFCount				.byte	; lines left in this playfield segment
+
+SpritePtr			.word
+ColorPtr			.word
+
 XPos					.byte
 YPos					.byte
+
 Tempx					.byte
+Tempa					.byte
+Bit2p0				.byte
+Colp0					.byte
+YP0						.byte
 
 					seg Code
 					org $f000
 
 start:		CLEAN_START
-					lda #5
-					sta XPos
+					lda #<PlayfieldData
+					sta PFPtr
+					lda #>PlayfieldData
+					sta PFPtr+1
+					lda #<sprDat
+					lda SpritePtr
+					lda #>sprDat
+					lda SpritePtr+1
+					lda #<SprCol
+					lda ColorPtr
+					lda #>SprCol
+					sta ColorPtr+1
+					lda #235
 					sta YPos
+					lda #39
+					sta XPos
 
-NFrame:
-					lsr SWCHB
+NFrame:		lsr SWCHB
 					bcc start
 					VERTICAL_SYNC
-					ldx XPos ; 87 XPos is when the extra scanline appears
-					cpx #$87
-					bpl exsnln
-					ldx #20
-					jmp LVBlank
+					TIMER_SETUP 37
+					lda #$88
+					sta COLUBK
+					lda #$5b
+					sta COLUPF
+					lda #$68
+					sta COLUP0
+					lda #1
+					sta CTRLPF			; Set Symetric Playfield Mode to on
+					lda #0
+					sta PFIndex
 
-exsnln:		ldx #19
-
-LVBlank:	sta WSYNC
-					dex
-					bne LVBlank
-
-					; Joystick code goes here
-MoveJoy:	lda #%00010000	; Player 0 up
-					bit SWCHA				; Test that bit
-					bne SkipUp			; Branch to SkipUp label if player is not pushing the joystick up
-					inc YPos				; Increment YPos
-
-SkipUp:		lda #%00100000	; Player 0 down
-					bit SWCHA				; Test that bit
-					bne SkipDn			; Branch to SkipUp label if player is not pushing the joystick down
-					dec YPos				; Increment YPos
-
-SkipDn:		lda #%01000000	; Player 0 left
-					bit SWCHA				; Test that bit
-					bne SkipLt			; Branch to SkipUp label if player is not pushing the joystick left
-					dec XPos				; Increment YPos
-
-SkipLt:		lda #%10000000	; Player 0 right
-					bit SWCHA				; Test that bit
-					bne SkipJoy			; Branch to SkipUp label if player is not pushing the joystick right
-					inc XPos				; Increment YPos
-
-SkipJoy:	lda XPos
+					lda YPos
+					sta YP0
+					lda XPos
 					ldx #0
 					jsr SetHorizPos
 					sta WSYNC
 					sta HMOVE
 
-					ldx #214
+					TIMER_WAIT
+					lda #0
+					sta VBLANK
 
-LVScan:		txa
-					sta Tempx
-					lda #$26
-					sta COLUPF ; Color of the playfield
-					lda Tempx
-					sta PF0
+					TIMER_SETUP 192
+					SLEEP 10
+
+NewPFSegment:
+					ldy PFIndex
+					lda (PFPtr),y
+					beq NoMoreSegs
+					sta PFCount
+
+					iny
+					lda (PFPtr),y
+					tax
+					iny
+					lda (PFPtr),y
+					sta Tempa
+					iny
+					lda (PFPtr),y
+					iny
+					sty PFIndex
+					tay
+
+					sta WSYNC
+					stx PF0
+					lda Tempa
 					sta PF1
-					sta PF2
-					stx COLUBK ; Color of the background
-					sec
-					sbc YPos
-					cmp SpriteHeight				; If the SprightHeight is greater than the Y position
-					bcc InSprite
+					lda Bit2p0
+					sta GRP0
+					sty PF2
+
+					ldx PFCount
+
+KernelLoop:
+					lda #SpriteHeight
+					inc YP0
+					sbc YP0
+					bcs DoDraw
 					lda #0
 
-InSprite:	tay											; Transfer the A register's value to the Y register
-					lda sprDat,Y						; Load bitmap data
-					sta WSYNC								; Wait for sync
-					sta GRP0								; Set the sprite bitmap
-					lda SprCol,Y						; Load color data
-					sta COLUP0							; Set the sprite color
+DoDraw:		pha
+					tay
+					lda (ColorPtr),y
+					sta Colp0
 
+					pla
+					asl
+					tay
+					lda (SpritePtr),y
+					sta Bit2p0
+					iny
+					lda (SpritePtr),y
+
+					sta WSYNC
+					sta GRP0
+					lda Colp0
+					sta COLUP0
 					dex
-					bne LVScan
+					beq NewPFSegment
 
-				; 43 lines of overscan
-					ldx #22
-LVOver:		sta WSYNC
-					dex
-					bne LVOver
+					sta WSYNC
+					lda Bit2p0
+					sta GRP0
+					jmp KernelLoop
 
-				; Cycle the sprite colors for the next frame
+NoMoreSegs
+					lda #0
+					sta COLUBK
+
+					TIMER_WAIT
+
+					TIMER_SETUP 29
+					lda #2
+					sta VBLANK
+					jsr MoveJoy
+					TIMER_WAIT
 					jmp NFrame
 
 SetHorizPos:
 					sta WSYNC
+					bit 0
 					sec
 DivideLoop:
 					sbc #15
@@ -120,6 +170,66 @@ DivideLoop:
 					sta HMP0,x
 					rts
 
+					; Joystick code goes here
+MoveJoy:	ldx YPos
+					lda #%00010000	; Player 0 up
+					bit SWCHA				; Test that bit
+					bne SkipUp			; Branch to SkipUp label if player is not pushing the joystick up
+					cpx #175
+					bcc SkipUp
+					inx
+
+SkipUp:		lda #%00100000	; Player 0 down
+					bit SWCHA				; Test that bit
+					bne SkipDn			; Branch to SkipUp label if player is not pushing the joystick down
+					cpx #1
+					bcs SkipDn
+					dex
+
+SkipDn:		stx YPos
+					ldx XPos
+					lda #%01000000	; Player 0 left
+					bit SWCHA				; Test that bit
+					bne SkipLt			; Branch to SkipUp label if player is not pushing the joystick left
+					cpx #254
+					bcc SkipLt
+					dex
+
+SkipLt:		lda #%10000000	; Player 0 right
+					bit SWCHA				; Test that bit
+					bne SkipJoy			; Branch to SkipUp label if player is not pushing the joystick right
+					cpx #1
+					bcc SkipJoy
+					inx
+
+SkipJoy:	stx XPos
+					rts
+
+PlayfieldData:
+        	.byte 13,#%00000000,#%00000000,#%00000000
+
+        	.byte 04,#%00000000,#%00011000,#%00011000
+        	.byte 04,#%00000000,#%00111100,#%00111100
+        	.byte 04,#%00000000,#%01111110,#%01111110
+        	.byte 04,#%00000000,#%00111100,#%00111100
+
+        	.byte 9,#%00000000,#%00000000,#%00000000
+
+        	.byte 04,#%00000000,#%00011000,#%00011000
+        	.byte 04,#%00000000,#%00111100,#%00111100
+        	.byte 04,#%00000000,#%01111110,#%01111110
+        	.byte 04,#%00000000,#%00111100,#%00111100
+
+        	.byte 9,#%00000000,#%00000000,#%00000000
+
+        	.byte 04,#%00000000,#%00011000,#%00011000
+        	.byte 04,#%00000000,#%00111100,#%00111100
+        	.byte 04,#%00000000,#%01111110,#%01111110
+        	.byte 04,#%00000000,#%00111100,#%00111100
+
+        	.byte 16,#%00000000,#%00000000,#%00000000
+        	.byte 0
+
 ; The sprite is flipped as that's how it works in the routine
 
 sprDat:		.byte #0        ; zero padding, also clears register
@@ -132,7 +242,7 @@ sprDat:		.byte #0        ; zero padding, also clears register
         	.byte #%10000001
         	.byte #%11111111
 
-; Cat-head color data
+; Sprite color data
 SprCol:		.byte #0        ; unused (for now)
         	.byte #$90
         	.byte #$92
